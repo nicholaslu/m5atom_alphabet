af0e6e7e4ca9b077013da78b10a6cf68e4195f4f
nicholaslu <nicholaslu1996@gmail.com>
2023-02-06 01:48:42 +0900

    Add lowercase letter support

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
-# m5atom_alphabet
+# Alphabet support for M5Atom
 Show alphabet on the 5x5 screen of M5Stack ATOM Matrix.
 
+## Hardware
+M5Stack ATOM Matrix
+
+## Framework
+Arduino
+
+## Environnment
+PlatformIO on VSCode
+
+## Usage:
+### 1. Include header file
+
+```cpp
+#include "M5Atom.h"
+#include "alphabet.h"
+```
+
+### 2. Instantiation
+```cpp
+Alphabet screen;
+```
+
+### 3. Initialize in `setup` function
+```cpp
+screen.Init();
+```
+or
+```cpp
+M5.begin(true, false, true);
+delay(200);
+```
+
+### 4. Display in `loop` function
+- Display letters, numbers, symbols and graphs
+```cpp
+screen.Display('A');
+delay(200);
+screen.Display('a');
+delay(200);
+screen.Display(Up);
+delay(200);
+screen.Display(WifiConnected);
+```
+- Change colors
+```cpp
+screen.SetColor(0xff7fe4);
+screen.Display('L');
+```
+or
+```cpp
+screen.SetColor(0xff, 0x7f, 0xe4);
+screen.Display('L');
+```
+or
+```cpp
+screen.Display('L', 0xff7fe4);
+```
+> The color of graph can't be changed.
+
+## Dependence
+- M5Atom
+- FastLED
--- lib/alphabet/alphabet.h
+++ lib/alphabet/alphabet.h
@@ -1,35 +1,35 @@
 #ifndef _ALPHABET_H_
 #define _ALPHABET_H_
 
-#define HEIGHT 5
-#define WIDTH 5
-#define SHAPE_SIZE 25
-#define GRAPH_SIZE 77
+#define kHeight 5
+#define kWidth 5
+#define kShapeSize 25
+#define kGraphSize 77
 
 typedef enum{
-    FULL,
-    EMPTY,
-    MARU,
-    BATSU,
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
+    Full,
+    Empty,
+    Maru,
+    Batsu,
+    Up,
+    Down,
+    Left,
+    Right,
 } extern_shape_t;
 
 typedef enum{
-    WIFI_CONNECTED,
-    WIFI_FAILURE,
-    WIFI_CONNECTING_1,
-    WIFI_CONNECTING_2,
-    WIFI_CONNECTING_3,
-    DJI,
-    DJI_D,
-    DJI_J,
-    DJI_I,
-    BT,
-    ROS,
-    micro_ROS,
+    WifiConnected,
+    WifiFailure,
+    WifiConnecting1,
+    WifiConnecting2,
+    WifiConnecting3,
+    Dji,
+    DjiD,
+    DjiJ,
+    DjiI,
+    Bluetooth,
+    Ros,
+    MicroRos,
     TokyoTech,
 } extern_graph_t;
 
@@ -37,24 +37,24 @@ class Alphabet{
     public:
         // variables
         unsigned char color[3] = {0xff, 0xff, 0xff};
-        bool shape[SHAPE_SIZE];
-        unsigned char graph[GRAPH_SIZE];
-        // methods
-        void init(void);
-        void set_text(char);
-        void set_text(int);
-        void gen_shape(int index, bool *shapes);
-        void gen_graph(void);
-        void set_color(unsigned char r, unsigned char g, unsigned char b);
-        void set_color(unsigned long rgb);
-        void display(void);
-        void display(char letter);
-        void display(char letter, unsigned long rgb);
-        void display(int number);
-        void display(int number, unsigned long rgb);
-        void display(extern_shape_t extern_shape);
-        void display(extern_shape_t extern_shape, unsigned long rgb);
-        void display(extern_graph_t extern_graph);
+        bool shape[kShapeSize];
+        unsigned char graph[kGraphSize];
+        // functions
+        void Init(void);
+        void SetText(char);
+        void SetText(int);
+        void GenShape(int index, const bool *shapes);
+        void GenGraph(void);
+        void SetColor(unsigned char r, unsigned char g, unsigned char b);
+        void SetColor(unsigned long rgb);
+        void Display(void);
+        void Display(char letter);
+        void Display(char letter, unsigned long rgb);
+        void Display(int number);
+        void Display(int number, unsigned long rgb);
+        void Display(extern_shape_t extern_shape);
+        void Display(extern_shape_t extern_shape, unsigned long rgb);
+        void Display(extern_graph_t extern_graph);
 };
 
 #endif
